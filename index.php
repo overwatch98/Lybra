4e4a023f4c6de788ff336f3cdf321cdf138fa279
Overwatch <mouhamedelbra@gmail.com>
2019-04-22 14:34:28 +0100

    first version of the index file

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+require 'vendor/autoload.php';
+if (session_status() === PHP_SESSION_NONE){
+    session_start();
+}
+try {
+
+    if(isset($_GET['url'])){
+        $url = $_GET['url'];
+    }else{
+        $url = 'accueil';
+    }
+    
+    if($url === 'accueil' || $url === 'accueil.html' || $url === 'home'){
+        require PAGE.'homeView.php';
+    }
+    elseif($url === 'professeur' || $url === 'professeur.html' || $url === 'prof'){
+        require PAGE . 'prof.php';
+    }
+    elseif($url === 'apropos' || $url === 'apropos.html' || $url === 'about'){
+        require PAGE . 'aboutView.php';
+    }
+    elseif($url === 'contact' || $url === 'contact.html' || $url === 'nous-contacter'){
+        require PAGE . 'contact.php';
+    }
+    elseif($url === 'mouhamedamine' || $url === 'quisuisje'){
+        require 'cv/cv.php';
+    }
+    elseif($url === 'login'){
+        require PAGE . 'loginView.php';
+    }
+    elseif($url === 'ajoutdeprof'){
+        require 'backend/addprof.php';
+    }
+    elseif($url === 'admin'){
+        require 'backend/home.php';
+    }
+    elseif($url === 'sedeconnecter'){
+        unset($_SESSION['auth']);
+        redirect('login');
+    }
+    else{
+        throw new Exception("La page que vous avez demandé n'existe pas ");
+    }
+}catch(Exception $e){
+    require PAGE . 'error/404.php';
+}
