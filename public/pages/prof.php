689751a789a6bbf5e3e8a30ac8c646cd4d0bb249
Overwatch <mouhamedelbra@gmail.com>
2019-04-27 13:23:18 +0100

    style de page de prof

--- public/pages/prof.php
+++ public/pages/prof.php
@@ -8,8 +8,8 @@ ob_start();
     <h1 class="text-center red-f mt-5 mb-5">Les professeurs du lycée de Boubara</h1>
     <div class="row">
         <?php foreach($profs AS $prof) : ?>
-            <div class="col-lg-4 col-md-4 col-sm-12">
-                <div class="card-prof p-4 mb-5 mt-2 text-center">
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <div class="card-prof p-4 mb-2 mt-2 text-center">
                     <div class="avatar-prof">
                         <?php if (!empty($prof->avatar)): ?>
                             <img class="img img-thumbnail" src="avatar/<?= $prof->avatar ?>" alt="avatarprof">
